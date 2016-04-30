ff5a97f15480cf579576ca33453047869c62a827
Alanyeh <alanyeh20001@hotmail.com>
2016-04-30 20:55:43 +0800

    add name column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430124955) do
+ActiveRecord::Schema.define(version: 20160430125358) do
 
   create_table "sentences", force: :cascade do |t|
     t.text     "content"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 20160430124955) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
