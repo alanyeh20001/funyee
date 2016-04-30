a10fc6aa1da5a29a42e665d967a083fdf517c117
Alanyeh <alanyeh20001@hotmail.com>
2016-04-30 20:57:42 +0800

    add omniauth provider & uid column to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430125358) do
+ActiveRecord::Schema.define(version: 20160430125700) do
 
   create_table "sentences", force: :cascade do |t|
     t.text     "content"
@@ -37,6 +37,8 @@ ActiveRecord::Schema.define(version: 20160430125358) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.string   "provider"
+    t.string   "uid"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
