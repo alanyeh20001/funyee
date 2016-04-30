c07be78253a0fee848f66b32abbc68e6370fc811
Alanyeh <alanyeh20001@hotmail.com>
2016-04-30 16:10:12 +0800

    shut the auto-generating stylsheets, javascripts, helper off

--- config/application.rb
+++ config/application.rb
@@ -31,5 +31,12 @@ module Funyee
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    
+    # Do not generate these files automatically.
+    config.generators do |g|
+      g.stylesheets false
+      g.javascripts false
+      g.helper false
+    end
   end
 end
