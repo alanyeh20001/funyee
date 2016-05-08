ef5e716f4987c5b839a6ce228d6b211bc15e6521
DumDumGeniuss <dumdumgenius@gmail.com>
2016-05-08 18:10:46 +0800

    Beautify the index page

---
+++ public/gulp/src/javascript/controller/topics_ctrl.js
@@ -0,0 +1,4 @@
+funyeeApp.controller("topicsCtrl", ["$scope", function($scope) {
+    this.datas= ["1", "2", "3", "4", "5", "6"];
+    this.title= "title";
+}]);
--- public/gulp/src/javascript/main.js
+++ public/gulp/src/javascript/main.js
@@ -1,15 +1,17 @@
 var funyeeApp = angular.module("funyeeApp", ["ui.router"]);
 
-funyeeApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
-
-    $urlRouterProvider.otherwise("/");
-
-	$stateProvider
-        .state('index', {
-            url: "/",
-            views: {
-            	"viewA": {templateUrl: "/templates/navbar.html"},
-            	"viewB": {templateUrl: "/templates/topics.html"}
-            }
-        });
-}]);
+(function() {
+    funyeeApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    
+        $urlRouterProvider.otherwise("/");
+    
+    	$stateProvider
+            .state('index', {
+                url: "/",
+                views: {
+                	"viewA": {templateUrl: "/templates/navbar.html"},
+                	"viewB": {templateUrl: "/templates/topics.html"}
+                }
+            });
+    }]);
+}());
