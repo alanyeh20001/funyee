db29eb941b15a20546c4f7399f4093214210d04f
DumDumGeniuss <dumdumgenius@gmail.com>
2016-05-13 23:17:11 +0800

    Fix gulp.js , install bower package , beautify views

--- gulp/src/javascript/controller/topics_ctrl.js
+++ gulp/src/javascript/controller/topics_ctrl.js
@@ -1,4 +1,18 @@
-funyeeApp.controller("topicsCtrl", ["$scope", function($scope) {
-    this.datas= ["1", "2", "3", "4", "5", "6"];
-    this.title= "title";
-}]);
+(function() {
+    funyeeApp
+        .controller("topicsCtrl", ["$scope", "sentenceServ",
+        	function($scope, sentenceServ) {
+                this.datas= ["1", "2", "3", "4", "5", "6", "7"];
+                this.title= "title";
+
+                var Sentence = sentenceServ.getRestfulModel();
+                console.log(Sentence.query());
+                console.log(Sentence.get({id: 1}));
+                var newSentence = new Sentence();
+                newSentence.content = "Hello Bae !";
+                newSentence.language = "Japanese";
+                newSentence.is_question = true;
+                newSentence.$save();
+            }
+        ]);
+}());
--- gulp/src/javascript/main.js
+++ gulp/src/javascript/main.js
@@ -1,21 +1,70 @@
-var funyeeApp = angular.module("funyeeApp", ["ui.router"]);
+var funyeeApp = angular.module("funyeeApp", ["ui.router", "pascalprecht.translate", "ngResource"]);
 
 (function() {
-    funyeeApp.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+	'use strict';
+
+	funyeeApp
+	    .constant("API_URL", "http://localhost:3000/");
+})
+
+(function() {
+    'use strict';
+
+    var en_US = {
+        "user.login": "Login",
+        "user.pickOne": "Sign up with",
+        "home": "home",
+        "language.english": "english",
+        "language.chinese": "chinese",
+        "question.ask": "Ask"
+    };
     
-        $urlRouterProvider.otherwise("/");
+    var zh_TW = {
+        "user.login": "登入",
+        "user.pickOne": "選擇登入方式",
+        "home": "主頁",
+        "language.english": "英文",
+        "language.chinese": "中文",
+        "question.ask": "提問"
+    };
+
+    funyeeApp
+        .config(['$translateProvider', 
+        	function ($translateProvider) {
+
+                $translateProvider.translations('en_US', en_US);
+                $translateProvider.translations('zh_TW', zh_TW);
+                $translateProvider.preferredLanguage('zh_TW');
+                //$translateProvider.useSanitizeValueStrategy('sanitize');
+            }
+        ]);
+
+}());
+
+(function() {
+    funyeeApp
+        .config(['$stateProvider', '$urlRouterProvider', 
+            function($stateProvider, $urlRouterProvider) {
     
-    	$stateProvider
-            .state('topics', {
-                url: "/",
-                views: {
-                	"viewA": {templateUrl: "dist/templates/topics.html"}
-                }
-            }).state('user', {
-                url: "/user",
-                views: {
-                	"viewA": {templateUrl: "dist/templates/user.html"}
-                }
-            });
-    }]);
+                $urlRouterProvider.otherwise("/");
+            
+            	$stateProvider
+                    .state('topics', {
+                        url: "/",
+                        views: {
+                        	"viewA": {templateUrl: "dist/templates/topics.html"}
+                        }
+                    }).state('login', {
+                        url: "/login",
+                        views: {
+                        	"viewA": {templateUrl: "dist/templates/user/login.html"}
+                        }
+                    }).state('login.test1', {
+                    	url: "/test",
+                        views: {
+                        	"test1@login": {templateUrl: "dist/templates/user/test.html"}
+                        }
+                    });
+            }
+        ]);
 }());
